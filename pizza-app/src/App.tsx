33e09ba50e755cc3a23edd97b707aef88df68066
EdKhismatov <E.Khismatov@yandex.ru>
2024-08-20 14:54:37 +0200

    route

--- pizza-app/src/App.tsx
+++ pizza-app/src/App.tsx
@@ -1,6 +1,29 @@
 import { MouseEvent ,useState } from 'react';
 import Button from './components/Button/Button';
 import Input from './components/Input/Input';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Menu from './pages/Menu/Menu';
+import Cart from './pages/Cart/Cart';
+import Error from './pages/Error/Error';
+
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Menu/>
+  },
+  {
+    path: '/cart',
+    element: <Cart/>
+  },
+  {
+    path: '*',
+    element: <Error/>
+  },
+
+])
+
 
 function App() {
   const [counter, setCounter] = useState<number>(0);
@@ -14,6 +37,11 @@ function App() {
       <Button onClick={addCounter}> Кнопка </Button>
       <Button appearence='big' onClick={addCounter}> Кнопка </Button>
       <Input placeholder='Email'/>
+      <div>
+        <a href='/'> Menu</a>
+        <a href='/cart'> Корзина</a>
+      </div>
+     <RouterProvider router={router}/>
     </>
   );
 }
